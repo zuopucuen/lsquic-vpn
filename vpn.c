ec54bf2e65ce43f9758c0bdf73e4dd5f699b1ac7
zuopucun <zuopucun@newlink.com>
2024-05-23 10:20:41 +0800

    enable stream flush

--- vpn.c
+++ vpn.c
@@ -246,7 +246,7 @@ vpn_on_write (lsquic_stream_t *stream, lsquic_stream_ctx_t *st_h)
     if(st_h->conn_h->vpn_ctx->tun_read_ev && BUFF_SIZE > BUFF_SIZE/4 + st_h->buf_off)
         event_add(st_h->conn_h->vpn_ctx->tun_read_ev, NULL);
 
-    //lsquic_stream_flush(stream);
+    lsquic_stream_flush(stream);
     lsquic_stream_wantwrite(stream, 0);
     lsquic_stream_wantread(stream, 1);
 }
