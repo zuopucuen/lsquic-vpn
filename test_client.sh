f4f0390a33e4c256223c7b5db3121c78fe69b872
zuopucun <zuopucun@leftdeMacBook-Pro-2.local>
2024-10-15 16:15:25 +0800

    delete test

--- test_client.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./vpn_client -c certs/client.crt -k certs/ca-key.pem -C certs/ca-cert.crt -s 127.0.0.1:4433 -L debug
--- test_server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./vpn_server -c certs/server.crt -k certs/ca-key.pem -C certs/ca-cert.crt -s 0.0.0.0:4433 -L debug
